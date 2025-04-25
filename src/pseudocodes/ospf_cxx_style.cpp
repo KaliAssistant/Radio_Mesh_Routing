a218edc008e1d76967524cec326d396f13b54aea
KaliAssistant <1325077@gmail.com>
2025-04-25 16:14:33 +0800

    update ospf pseudocode

--- src/pseudocodes/ospf_cxx_style.cpp
+++ src/pseudocodes/ospf_cxx_style.cpp
@@ -6,9 +6,15 @@
  */
 
 
-#define TYPE_L2LAYER_MAGICCODE 0xC6, 0x5A   // Header Type Magic Code For DataLink header 0xC65A
-#define TYPE_HELLOPACKET_MAGICCODE  0x9A, 0xB8   // Packet Type Magic Code For HelloPacket 0x9AB8
+#include <iostream>
+#include <stdio.h>
 
+
+const uint8_t TYPE_L2LAYER_MAGICCODE[2] = {0xC6, 0x5A};       // Header Type Magic Code For DataLink header 0xC65A
+const uint8_t TYPE_L3LAYER_MAGICCODE[2] = {0xCC, 0x4A};       // Header Type Magic Code For NetWorking header 0xCC4A
+const uint8_t TYPE_HELLOPACKET_MAGICCODE[2] = {0x9A, 0xB8};   // Packet Type Magic Code For HelloPacket 0x9AB8
+const uint8_t TYPE_L3PACKET_HEADER_CLEAR[2] = {0xCC, 0xAA};   // L3 Header 0x05-0x06 Packet Type Magic code For Non-Encrypted 0xCCAA
+const uint8_t TYPE_L3PACKET_HEADER_CRYPT[2] = {0xBB, 0xDD};   // L3 Header 0x05-0x06 Packet Type Magic code For Encrypted 0xBBDD
 
 /*
  *   L2 Header
@@ -24,47 +30,109 @@
 
 
 /*
- *   L3 Packet
+ *   L3 Header
+ *   
+ *   +--------------------------- L3 Header ------------------------------+
+ *   +    00  01  02  03  04  05  06  07  08  09  0A  0B  0C  0D  0E  0F  +
+ *   +--------------------------------------------------------------------+
+ *   + 0| .N  .E  .T  CC  4A [-*TYP-][------- 8 byte key hint ------] FF |+
+ *   +--------------------------------------------------------------------+
+ *   
+ *   *TYP: Should be `const uint8_t TYPE_L3PACKET_HEADER_XX[2]`
+ *
+ *   TOTAL 16 Bytes
+ */
+
+
+/*
+ *   L2.5 Packet (L2+L3)
+ *   
+ *   +-------------------------- L2.5 Packet -----------------------------+
+ *   +    00  01  02  03  04  05  06  07  08  09  0A  0B  0C  0D  0E  0F  +
+ *   +--------------------------------------------------------------------+
+ *   + 0|[------------------------ L2 Header ---------------------------]|+
+ *   + 1|[-------------------------L3 Header ---------------------------]|+
+ *   + 2|[---------------- L3 Packet Data (Max 208 Bytes) -------------- |+
+ *   + ~| ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ |+
+ *   + E| --------------------------------------------------------------]|+
+ *   + F|[-6 byte L2.5 CheckSum-] FF  x   x   x   x   x   x   x   x   x  |+
+ *   +--------------------------------------------------------------------+
+ *
+ *   MAX 247 Bytes
+ */
+ 
+
+/*
+ *   Hello Packet (L3)
  *
  *   +-------------------------- Hello Packet ----------------------------+
  *   +    00  01  02  03  04  05  06  07  08  09  0A  0B  0C  0D  0E  0F  +
  *   +--------------------------------------------------------------------+
- *   + 0| .N  .E  .T  9A  B8 [--------------------- 16 byte Node Address |+
- *   + 1| ------------------][------------------------------------------ |+
- *   + 2| -------------------- 32 byte Public Key ---------------------- |+
- *   + 3| ------------------][- 4 byte seq -][- 4 byte tmp -][- TTL-] FF |+
- *   + 4| .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .  |+
- *   + 5| .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .  |+
- *   + 6| .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .  |+
- *   + 7| .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .  |+
+ *   + 0| 9A  B8 [--------------------- 16 byte Node Address ----------- |+
+ *   + 1| ------][------------------------------------------------------ |+
+ *   + 2| 32 byte Public Key ------------------------------------------- |+
+ *   + 3| ------][- 4 byte seq -][- 4 byte tmp -][- TTL-][- RandID -] FF |+
  *   +--------------------------------------------------------------------+
  *
  *   TOTAL 64 Bytes
  */
 
-typedef struct NodeID { uint64_t x[2]; } NodeID;   // 128-bit MAC or UID
-typedef struct PubKey { uint8_t x[32]; } PubKey;   // 256-bit Public Key
-typedef struct PrivKey { uint8_t x[32]; } PrivKey; // 256-bit Private Key
-typedef struct AESKey { uint8_t x[32]; } AESKey;   // 256-bit AES Shared Key
-typedef struct ChkSum10 { uint8_t x[10];} ChkSum10; // 10 Bytes CheckSum 
+typedef struct NodeID { uint64_t x[2]; } NodeID;    // 128-bit MAC or UID
+typedef struct PubKey { uint8_t x[32]; } PubKey;    // 256-bit Public Key
+typedef struct PrivKey { uint8_t x[32]; } PrivKey;  // 256-bit Private Key
+typedef struct AESKey { uint8_t x[32]; } AESKey;    // 256-bit AES Shared Key
+typedef struct ChkSum10 { uint8_t x[10];} ChkSum10; // 10 Bytes CheckSum
+typedef struct ChkSum8 { uint8_t x[8];} ChkSum8     // 8 Bytes CheckSum
+typedef struct KeyHint8 { uint8_t x[8];} KeyHint8;  // 8 Bytes Key Hint 
 
 
 struct L2Header {
     /* 16 Bytes */
-    const header[5] = {0x44, 0x41, 0x4C, TYPE_L2LAYER_MAGICCODE};                         // L2: 0x00-0x04
+    const uint8_t header[5] = {
+      'D', 'A', 'L',
+      TYPE_L2LAYER_MAGICCODE[0],
+      TYPE_L2LAYER_MAGICCODE[1]
+    };                                                                                    // L2: 0x00-0x04
     ChkSum10 L3ChkSum;                                                                    // L2: 0x05-0x0E
-    const uint8_t headEnd = 0xFF;                                                         // L2: 0x0F
+    const uint8_t headerEnd = 0xFF;                                                       // L2: 0x0F
 };
+
+struct L3Header {
+    /* 16 Bytes */
+    const uint8_t header[5] = {
+      'N', 'E', 'T',
+      TYPE_L3LAYER_MAGICCODE[0],
+      TYPE_L3LAYER_MAGICCODE[1]
+    };                                                                                    // L3: 0x00-0x04
+    uint8_t pktType[2];                                                                   // L3: 0x05-0x06
+    KeyHint8 key_hint;                                                                    // L3: 0x07-0x0E
+    const uint8_t headEnd = 0xFF;                                                         // L3: 0x0F
+};
+
+struct L2D5Packet {
+    /* 247 Bytes */
+    L2Header l2_header;                                                                   // L2.5: 0x00-0x0F
+    L3Header l3_header;                                                                   // L2.5: 0x10-0x1F
+    uint8_t pktData[208];                                                                 // L2.5: 0x20-0xEF
+    ChkSum8 L2D5ChkSum;                                                                   // L2.5: 0xF0-0xF5
+    const uint8_t pktEnd = 0xFF;                                                          // L2.5: 0xF6
+};
+
+
 
 struct HelloPacket {
     /* 64 Bytes */
-    const uint8_t header[5] = {0x4E, 0x45, 0x54, TYPE_HELLOPACKET_MAGICCODE};             // L3: 0x00-0x04
-    NodeID sender_id;                                                                     // L3: 0x05-0x14
-    PubKey pubkey;                                                                        // L3: 0x15-0x34
-    uint32_t seq_no;                                                                      // L3: 0x35-0x38
-    uint32_t timestamp;                                                                   // L3: 0x39-0x3C
-    uint8_t ttl_MAX_Nodes;  //  Hops limit, MAX 255 (1 Byte)                              // L3: 0x3D
-    uint8_t ttl_CNT_Nodes;  //  Hops count, sender is 0. When CNT=MAX, drop the packet.   // L3: 0x3E
+    const uint8_t header[2] = {
+      TYPE_HELLOPACKET_MAGICCODE[0],
+      TYPE_HELLOPACKET_MAGICCODE[1]
+    };                                                                                    // L3: 0x00-0x01
+    NodeID sender_id;                                                                     // L3: 0x02-0x11
+    PubKey pubkey;                                                                        // L3: 0x12-0x31
+    uint32_t seq_no;                                                                      // L3: 0x32-0x35
+    uint32_t timestamp;                                                                   // L3: 0x36-0x39
+    uint8_t ttl_MAX_Nodes;  //  Hops limit, MAX 255 (1 Byte)                              // L3: 0x3A
+    uint8_t ttl_CNT_Nodes;  //  Hops count, sender is 0. When CNT=MAX, drop the packet.   // L3: 0x3B
+    uint8_t random_id[3];   //  3 byte random UID                                         // L3: 0x3C-0x3E
     const uint8_t pktEnd = 0xFF;                                                          // L3: 0x3F
 };
 
@@ -88,3 +156,11 @@ struct RemoteNodes {
     uint32_t last_seen;
 };
 
+
+
+
+int main() {
+  L2Header l2test;
+  printf("%x, %x, %x, %x, %x", l2test.header[0], l2test.header[1], l2test.header[2], l2test.header[3], l2test.header[4]);
+  return 0;
+}
